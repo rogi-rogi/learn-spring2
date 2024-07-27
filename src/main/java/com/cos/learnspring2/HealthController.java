c66b3f3cedec51d3e69e5782d50c96ac70bfb862
rogi-rogi <yrkim6883@gmail.com>
2024-07-27 18:02:21 +0900

    first commit

---
+++ src/main/java/com/cos/learnspring2/HealthController.java
@@ -0,0 +1,37 @@
+package com.cos.learnspring2;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+@RestController
+public class HealthController {
+    @Value("${server.env}")
+    private String env;
+    @Value("${server.port}")
+    private String serverPort;
+    @Value("${server.serverAddress}")
+    private String serverAddress;
+    @Value("${serverName}")
+    private String serverName;
+
+
+    @GetMapping("/hc")
+    public ResponseEntity<?> healthCheck() {
+        Map<String, String> response = new TreeMap<>();
+        response.put("serverName", serverName);
+        response.put("serverAddress", serverAddress);
+        response.put("serverPort", serverPort);
+        response.put("env", env);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/env")
+    public ResponseEntity<?> getEnv() {
+        return ResponseEntity.ok().body(env);
+    }
+}
---
+++ src/main/java/com/cos/learnspring2/LearnSpring2Application.java
@@ -0,0 +1,13 @@
+package com.cos.learnspring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpring2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpring2Application.class, args);
+    }
+
+}
